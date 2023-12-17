982416561a963a8b1ee00e0de0a0b87462d5abea
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-12-17 12:29:06 -0300

    Added permission checker support for ban command

--- Bilbo/Commands/Moderation/Ban.cs
+++ Bilbo/Commands/Moderation/Ban.cs
@@ -8,36 +8,38 @@ namespace Bilbo.Commands.Moderation;
 public class Ban : CommandFramework
 {
     private readonly DiscordSocketClient _client;
-    
+
     private new static readonly List<Option> Options = new()
     {
         new Option("user", ApplicationCommandOptionType.User, "The user to ban", true),
         new Option("reason", ApplicationCommandOptionType.String, "The reason for the ban", false),
         new Option("days", ApplicationCommandOptionType.Integer, "The number of days of messages to delete", false)
     };
-    
+
     public Ban(DiscordSocketClient client) : base("ban", "Ban a user from the server", Options)
     {
         _client = client;
     }
 
-    // TODO: Role based permissions(Ex: mods can ban only with the bot) 
+    // TODO: Role based permissions(Ex: mods can ban only with the bot)
     // TODO: Command response on a specific channel(Configurable at the website)
     public override void CommandAction(SocketSlashCommand command)
     {
-        if (command.User is not SocketGuildUser commandUser || !commandUser.GuildPermissions.BanMembers)
+        var authorPermissionChecker =
+            new AuthorPermissionChecker(command, new[] { GuildPermission.BanMembers }, null, null);
+
+        if (!authorPermissionChecker.HasPermissions)
         {
-            command.RespondAsync("You do not have permission to use this command.");
             return;
         }
-        
+
         var user = command.Data.Options.First().Value as SocketGuildUser;
         var reason = command.Data.Options.ElementAt(1).Value as string;
         var days = command.Data.Options.Last().Value as int? ?? 0;
         var guild = _client.GetGuild(command.GuildId!.Value);
 
         var embedBuilder = new CustomEmbedBuilder();
-        
+
         embedBuilder.WithTitle("🔨 Ban");
         embedBuilder.WithDescription($"You have been banned from {guild}.");
         embedBuilder.AddField("Reason: ", reason ?? "No reason provided.");
@@ -49,7 +51,7 @@ public class Ban : CommandFramework
 
         user.SendMessageAsync("", false, embedBuilder.Build());
         user.BanAsync(days, reason);
-        
+
         embedBuilder.WithDescription($"{user?.Mention} has been banned.");
         command.RespondAsync("", new[] { embedBuilder.Build() });
     }
